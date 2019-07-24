dc987825664be3fee34eeba869c867638abfaba6
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-24 15:56:52 -0500

    variable exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,49 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
-        System.out.print("Hello, Betelguese.");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Betelguese.");
+
+        int myFavoriteNumber = 4;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "How's it going dudes";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+//        System.out.println(three);
+
+//        int x = 4;
+//        x = x + 5;
+//        int x = 4 + 5;
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -=x;
     }
 }
