a3e7c7397f0cf9fc06107cd0340be463ef35ebd5
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-26 17:01:29 -0500

    small update

--- src/Bob.java
+++ src/Bob.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Bob {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("You're walking in the woods. There's no one around and your phone is dead.\nOut of the corner of your eye you spot him.\nBob.");
+
         do {
-            System.out.println("You come across the teenager Bob. What do you say to him?");
+            System.out.println("Do you want to try saying something to him?");
             String answer = scanner.nextLine();
 
             if (answer.endsWith("?")) {
                 System.out.println("Bob replies with \"Sure.\"");
             } else if (answer.endsWith("!")) {
                 System.out.println("Bob replies with \"Whoa, chill out!\"");
-            } else if (answer.equals("")) {
+            } else if (answer.isEmpty()) {
                 System.out.println("Bob replies with \"Fine. Be that way!\"");
-            } else if (answer.equalsIgnoreCase("bye")) {
+            } else if (answer.contains("bye")) {
                 System.out.println("Bob replies with \"Later loser.\"");
                 break;
             } else {
