482853cea613afd14a5c7a34becaad178917ec8a
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-26 16:15:32 -0500

    string exercise complete

---
+++ src/StringExercise.java
@@ -0,0 +1,9 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = "We don't need no education\nWe don't need no thought control";
+        message += "Check \"this\" out!, \"s inside of \"s!";
+        message += "In windows, the main drive is usually C:\\";
+        message += "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslashes \\\\\\!";
+        System.out.println(message);
+    }
+}
