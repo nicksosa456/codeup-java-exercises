a59bbccde92a15033592a3fa10ff23718d029960
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-26 16:37:53 -0500

    bob exercise complete

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("You come across the teenager Bob. What do you say to him?");
+            String answer = scanner.nextLine();
 
+            if (answer.endsWith("?")) {
+                System.out.println("Bob replies with \"Sure.\"");
+            } else if (answer.endsWith("!")) {
+                System.out.println("Bob replies with \"Whoa, chill out!\"");
+            } else if (answer.equals("")) {
+                System.out.println("Bob replies with \"Fine. Be that way!\"");
+            } else if (answer.equalsIgnoreCase("bye")) {
+                System.out.println("Bob replies with \"Later loser.\"");
+                break;
+            } else {
+                System.out.println("Bob replies with \"Whatever.\"");
+            }
+        } while (true);
     }
 }
