5b37bccecb74dbca888bc620b0426bbf1331332b
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-30 10:27:45 -0500

    changed factorial exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -19,28 +19,54 @@ public class MethodsExercises {
 //    }
 
 //    number 2
-//    public static int getInteger(int min, int max){
-//        Scanner scanner = new Scanner(System.in);
-//        int userInput;
-//        do {
-//            System.out.println("Enter a number between "+min+" and "+max+": ");
-//            userInput = scanner.nextInt();
-//            if (userInput > max) {
-//                System.out.println("That is not between "+min+" and "+max);
-//            } else {
-//                System.out.println("You listened to the directions");
-//                break;
-//            }
-//        } while (true);
-//        return userInput;
-//    }
+    public static int getInteger(int min, int max){
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+        do {
+            System.out.println("Enter a number between "+min+" and "+max+": ");
+            userInput = scanner.nextInt();
+            if (userInput > max || userInput < min) {
+                System.out.println("That is not between "+min+" and "+max);
+            } else {
+                System.out.println("You listened to the directions");
+                break;
+            }
+        } while (true);
+        return userInput;
+    }
 
-//    static long factorial(long n){
+    private static void doFactorial() {
+        Scanner scan = new Scanner(System.in);
+        long fact = getInteger(1, 20);
+        System.out.println("Here is the factorial of that number:");
+        factorial(fact);
+        System.out.println();
+        do {
+            System.out.println("Do you want to see another factorial? [y/n]");
+            String answer = scan.next();
+            if (answer.equalsIgnoreCase("n")) {
+                break;
+            }
+        } while (true);
+    }
+
+    public static void factorial(long n){
+        long fact;
+        for (int i = 1; i <= n; i++) {
+            fact = 1;
+            System.out.print(i +"! = ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+                if (j != i) System.out.print(" x ");
+                fact *= j;
+            }
+            System.out.println(" = "+fact);
+        }
 //        if (n == 1)
 //            return 1;
 //        else
 //            return(n * factorial(n-1));
-//    }
+    }
 
 //    number 4
 //    static long rollDice(int x) {
@@ -61,6 +87,7 @@ public class MethodsExercises {
 //        getInteger(1, 10);
 
 //        number 3
+        doFactorial();
 //        do {
 //            System.out.println("Enter a number between 1 and 20 to see a factorial: ");
 //            long factorialInput = scan.nextInt();
